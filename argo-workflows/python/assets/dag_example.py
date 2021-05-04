1a77ea4d9ee4c96eb36b1caaad8c70c13515fd0f
terrytangyuan <terrytangyuan@gmail.com>
2021-05-04 09:40:14 -0400

    Add DAG example in Python tutorial
    Signed-off-by: terrytangyuan <terrytangyuan@gmail.com>

---
+++ argo-workflows/python/assets/dag_example.py
@@ -0,0 +1,42 @@
+import couler.argo as couler
+from couler.argo_submitter import ArgoSubmitter
+
+
+def job(name):
+    couler.run_container(
+        image="docker/whalesay:latest",
+        command=["cowsay"],
+        args=[name],
+        step_name=name,
+    )
+
+#     A
+#    / \
+#   B   C
+#  /
+# D
+def linear():
+    couler.set_dependencies(lambda: job(name="A"), dependencies=None)
+    couler.set_dependencies(lambda: job(name="B"), dependencies=["A"])
+    couler.set_dependencies(lambda: job(name="C"), dependencies=["A"])
+    couler.set_dependencies(lambda: job(name="D"), dependencies=["B"])
+
+#   A
+#  / \
+# B   C
+#  \ /
+#   D
+def diamond():
+    couler.dag(
+        [
+            [lambda: job(name="A")],
+            [lambda: job(name="A"), lambda: job(name="B")],  # A -> B
+            [lambda: job(name="A"), lambda: job(name="C")],  # A -> C
+            [lambda: job(name="B"), lambda: job(name="D")],  # B -> D
+            [lambda: job(name="C"), lambda: job(name="D")],  # C -> D
+        ]
+    )
+
+linear()
+submitter = ArgoSubmitter(namespace="argo")
+couler.run(submitter=submitter)
