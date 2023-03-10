3760a05f6fe55bcd70c0bbdd5b018647bcc8b9bd
Tim Collins <tim@thecollins.team>
2023-03-10 14:39:19 +0000

    move the git work to a background script

--- argo-workflows/workflow-examples/foreground.sh
+++ argo-workflows/workflow-examples/foreground.sh
@@ -1,8 +1,1 @@
-echo
-echo "Starting setup..."
-echo
-git clone https://github.com/argoproj/argo-workflows.git --depth 1 -q
-mv argo-workflows/examples/ . > /dev/null
-rm -rf argo-workflows > /dev/null
-
 curl -s https://raw.githubusercontent.com/pipekit/argo-workflows-intro-course/master/argo-workflows/install.sh|sh
