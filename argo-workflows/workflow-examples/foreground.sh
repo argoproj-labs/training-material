93ebab332d8072c7457e25fabca123005ba38bca
Tim Collins <tim@thecollins.team>
2023-03-10 14:35:44 +0000

    hide the install a little more

--- argo-workflows/workflow-examples/foreground.sh
+++ argo-workflows/workflow-examples/foreground.sh
@@ -1,3 +1,8 @@
-git clone https://github.com/argoproj/argo-workflows.git --depth 1 && mv argo-workflows/examples/ . && rm -rf argo-workflows
+echo
+echo "Starting setup..."
+echo
+git clone https://github.com/argoproj/argo-workflows.git --depth 1 -q
+mv argo-workflows/examples/ . > /dev/null
+rm -rf argo-workflows > /dev/null
 
 curl -s https://raw.githubusercontent.com/pipekit/argo-workflows-intro-course/master/argo-workflows/install.sh|sh
