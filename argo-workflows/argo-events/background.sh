4ee72af2d92b18d6b5e6d806935bc226015d55c4
Tim Collins <tim@thecollins.team>
2023-06-02 12:24:08 +0100

    try this way?

--- argo-workflows/argo-events/background.sh
+++ argo-workflows/argo-events/background.sh
@@ -4,12 +4,6 @@ wget https://raw.githubusercontent.com/tico24/argo-workflows-intro-course/master
 wget https://raw.githubusercontent.com/tico24/argo-workflows-intro-course/master/config/argo-events/minio-sensor.yaml
 wget https://raw.githubusercontent.com/tico24/argo-workflows-intro-course/master/config/argo-events/sa.yaml
 
-# Minio
-kubectl delete -f https://raw.githubusercontent.com/pipekit/argo-workflows-intro-course/master/config/minio/minio-deploy.yaml  > /dev/null
-kubectl create namespace minio
-#kubectl apply -f https://raw.githubusercontent.com/pipekit/argo-workflows-intro-course/master/config/minio-events/ -n minio
-kubectl apply -f https://raw.githubusercontent.com/tico24/argo-workflows-intro-course/master/config/minio-events/ -n minio
-
 # Install the minio client
 curl https://dl.min.io/client/mc/release/linux-amd64/mc \
   --create-dirs \
@@ -17,3 +11,9 @@ curl https://dl.min.io/client/mc/release/linux-amd64/mc \
 
 chmod +x /root/mc
 export PATH=$PATH:/root/
+
+# Minio
+kubectl delete -f https://raw.githubusercontent.com/pipekit/argo-workflows-intro-course/master/config/minio/minio-deploy.yaml  > /dev/null
+kubectl create namespace minio
+#kubectl apply -f https://raw.githubusercontent.com/pipekit/argo-workflows-intro-course/master/config/minio-events/ -n minio
+kubectl apply -f https://raw.githubusercontent.com/tico24/argo-workflows-intro-course/master/config/minio-events/ -n minio
